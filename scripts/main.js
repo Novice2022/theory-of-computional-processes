5320543bbf88a8ac65a281fe14120ed4e08d375e
Novice2022 <igor.zab.13@gmail.com>
2025-07-22 00:50:04 +0300

    create project and display the net (states and transitions - ok, arcs - todo)

---
+++ scripts/main.js
@@ -0,0 +1,60 @@
+function manageDom() {
+    const STATE_DIAMETER_PX = 35 + 10 * 2 + 2;
+    const TRANSITION_HEIGHT_PX = 6;
+    
+    const manageStatesLabels = () => {
+        let labels = document.getElementsByClassName('state-label');
+    
+        for (let i = 0; i < labels.length; i++) {
+            const label = labels.item(i);
+
+            const height = label.offsetHeight;
+            const width = label.offsetWidth;
+
+            if ([
+                'state-limit-to-out',
+                'state-in'
+            ].includes(label.parentElement.id)) {
+                label.style.top = `${ STATE_DIAMETER_PX + height }px`;
+                label.style.left = `-${ (width - STATE_DIAMETER_PX) / 2 }px`;
+            }
+            else if ([
+                'state-limit-to-in',
+                'state-ready-to-produce'
+            ].includes(label.parentElement.id)) {
+                label.style.left = `-${ (width - STATE_DIAMETER_PX) / 2 }px`;
+            } else {
+                label.style.top = `${ STATE_DIAMETER_PX / 2 + height / 2 }px`;
+                label.style.left = `-${ width + 10 }px`;
+            }
+
+            label.parentElement.style.marginTop = `-${height}px`;
+        }    
+    }
+    
+    const manageTransitionsLabels = () => {
+        let labels = document.getElementsByClassName('transition-label');
+    
+        for (let i = 0; i < labels.length; i++) {
+            const label = labels.item(i);
+
+            const height = label.offsetHeight;
+            const width = label.offsetWidth;
+
+            label.style.top = `${ TRANSITION_HEIGHT_PX / 2 + height / 2 }px`;
+            label.style.left = `-${ width + 10 }px`;
+
+            label.parentElement.style.marginTop = `-${height}px`;
+        }    
+    }
+    
+    manageStatesLabels();
+    manageTransitionsLabels();
+}
+
+
+function main() {
+    manageDom();
+}
+
+main()
